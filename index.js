b7eaac8017c131485f3eec8d00cb7ece8c1dbebf
Brad Harris <bmharris@gmail.com>
2016-04-25 17:33:30 -0600

    adding botByTeamId function

--- index.js
+++ index.js
@@ -11,6 +11,21 @@ exports.start = function (controller, config) {
   // expose botkit workers
   beepboop.workers = beepBoopBotkit.workers
 
+  // Returns a botkit bot by Slack team id
+  beepboop.botByTeamId = function (teamId) {
+    // Get a handle on the team's botkit worker/bot
+    var resourceId = Object.keys(beepboop.workers).filter(function (key) {
+      var entry = beepboop.workers[key]
+      return entry.resource && entry.resource.SlackTeamID === teamId
+    })[0]
+
+    if (!resourceId) {
+      return null
+    }
+
+    return beepboop.workers[resourceId].worker
+  }
+
   // return normal beepboop emitter
   return beepboop
 }
